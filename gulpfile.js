d38d8701dd00380139928a40d815b2bdad02c82b
Filip <filip.potocnik@gmail.com>
2016-09-19 20:13:08 +0200

    add gulp sass, run it..

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('bacon', function(){
   console.log('fak kok je dobra slanina, sick!');
+});
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
 });
